06bfab2564c66b6eec51a4cae50bcc7c3b5e4abc
Awaludin AR <awaludinar741@gmail.com>
2022-08-22 06:19:14 +0700

    Update quasar.config.js

--- quasar.config.js
+++ quasar.config.js
@@ -95,7 +95,7 @@ module.exports = configure(function (ctx) {
 
         // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#framework
         framework: {
-            config: {},
+            config: { notify: {} },
 
             // iconSet: 'material-icons', // Quasar icon set
             // lang: 'en-US', // Quasar language pack
@@ -108,7 +108,7 @@ module.exports = configure(function (ctx) {
             // directives: [],
 
             // Quasar plugins
-            plugins: [],
+            plugins: ["Notify"],
         },
 
         // animations: 'all', // --- includes all animations
